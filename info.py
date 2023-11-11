68adcadf1dcbe26f3eedd3a846dc00d9cb94bf71
f5fire445 <150526267+f5fire445@users.noreply.github.com>
2023-11-11 18:48:55 +0530

    info.py

--- info.py
+++ info.py
@@ -6,7 +6,7 @@ def is_enabled(value, default):
     if value.lower() in ["true", "yes", "1", "enable", "y"]:
         return True
     elif value.lower() in ["false", "no", "0", "disable", "n"]:
-        return False
+        return Falsetjttjhjcyjxtftttttjffgfgjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
     else:
         return default
 
